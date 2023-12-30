ee5ded2d65e0473e585ebe5ec3ab9d80aa28cd3b
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-12-30 15:57:25 +0530

    Create template of expense-tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 
 import './App.css'
+import {Header} from "./components/Header.tsx";
+import {Balance} from "./components/Balance.tsx";
+import {IncomeExpenses} from "./components/IncomeExpenses.tsx";
+import {TransactionList} from "./components/TransactionList.tsx";
+import {Form} from "./components/Form.tsx";
 
 function App() {
 
 
   return (
     <>
-
+      <Header/>
+        <div className="container">
+            <Balance/>
+            <IncomeExpenses/>
+            <TransactionList/>
+            <Form/>
+        </div>
     </>
   )
 }
